aad7b3cbf820c664bb225fcfcda9f8ac082cc734
Alexandr Bredikhin <akagami.beats@gmail.com>
2025-05-05 17:04:25 +0500

    upd

--- index.fe121.js
+++ index.fe121.js
@@ -1,4 +1,4 @@
-System.register(["./application.dc56c.js"], function (_export, _context) {
+System.register(["./application.4640f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
