e05ceb537733daa1c5406f199630f1c3b19e1651
Alexandr Bredikhin <akagami.beats@gmail.com>
2025-05-05 19:24:19 +0500

    upd

--- index.b7453.js
+++ index.b7453.js
@@ -1,4 +1,4 @@
-System.register(["./application.56bb6.js"], function (_export, _context) {
+System.register(["./application.3157d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
